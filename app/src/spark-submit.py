a9c73e8fcc8b22f64fa03469328ef899042df657
mimiski <mimiski@protonmail.com>
2022-05-03 11:44:56 +0200

    Move schema.json to app/

--- app/src/spark-submit.py
+++ app/src/spark-submit.py
@@ -1,14 +1,8 @@
 from pyspark import SparkConf, SparkContext
 
-conf = SparkConf().setAppName("stats calculation")
-sparkContext = SparkContext(conf=conf)
-sparkContext.setLogLevel("DEBUG")
 
-
-
-patientsRdd = sparkContext.parallelize(patients)
+df = spark.read.json("/app/data/*.log")
+df.printSchema()
+df.show()
 
 # https://stackoverflow.com/questions/34216390/how-to-see-the-contents-of-each-partition-in-an-rdd-in-pyspark/34216391
-triplesPerPartitionRdd = patientsRdd.glom().flatMap(lambda patientsInPartition: [p.getRdfTriples(N) for p in patientsInPartition])
-serializedRdd = triplesPerPartitionRdd.map(lambda triplesPerPartition: RDFSerializer.serialize(triplesPerPartition))
-serializedRdd.saveAsTextFile("serialized_" + str(N))
