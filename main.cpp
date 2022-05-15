1e3ca294f2698434d276a01aae848f7444d3c666
mimimi <mimiski@protonmail.com>
2022-05-15 19:49:37 +0200

    Fixed reading strings encoded with cp1252 (they are sanitized now)

--- main.cpp
+++ main.cpp
@@ -5,25 +5,36 @@
 #include <iostream>
 #include <fstream>
 
+#include <codecvt>
+#include <iostream>
+#include <locale>
+#include <string>
+#include "utils.h"
+
 using namespace std;
 
 int main(int argc, char **argv)
 {
+  std::ios::sync_with_stdio(false);
+
   auto file_names = vector<string>(argv, argv + argc);
-  auto jsons = file_names 
-    | std::views::drop(1) // the name of the program
-    | std::views::transform([](string fileName) { 
+  
+  auto jsons = file_names
+    | std::views::filter([](string s) { return s.ends_with(".log"); })
+    | std::views::transform([](string fileName) {
       ifstream file(fileName);
       std::stringstream buffer;
       buffer << file.rdbuf();
-      auto jsonContents = buffer.str();
-      auto parsedJson = nlohmann::json::parse(jsonContents); 
+      const auto jsonContents = buffer.str();
+      auto sanitizedJsonContents = CP1252_to_UTF8(jsonContents);
+      auto parsedJson = nlohmann::json::parse(sanitizedJsonContents);
       darklight::LogFile *data = new darklight::LogFile;
       nlohmann::from_json(parsedJson, *data);
       return data;
     });
 
-  auto first = jsons.front(); 
+  auto first = jsons.front();
+  // auto size = jsons.size(); 
 
   return 0;
 }
---
+++ utils.cpp
@@ -0,0 +1,15 @@
+#include "utils.h"
+
+using namespace std;
+
+std::string CP1252_to_UTF8(const std::string& byte_array) {
+
+  // Byte array => Unicode.
+  std::wstring unicode(byte_array.size(), ' ');
+  for (size_t i = 0; i < unicode.size(); ++i)
+    unicode[i] = CP1252_UNICODE_TABLE[(uint8_t)byte_array[i]];
+
+  // Unicode => UTF8.
+  std::wstring_convert<std::codecvt_utf8<wchar_t>> unicode_to_utf8;
+  return unicode_to_utf8.to_bytes(unicode);
+}
